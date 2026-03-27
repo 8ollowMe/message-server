9185598a73d369f7c124496fedb4dbfbabb07a2c
kjs0406 <rnjswlstjr19089@gmail.com>
2026-03-27 14:05:19 +0900

    feat: 초기 생성

---
+++ message-server/src/main/java/com/followMe/message_server/MessageServerApplication.java
@@ -0,0 +1,12 @@
+package com.followMe.message_server;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+public class MessageServerApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(MessageServerApplication.class, args);
+	}
+
+}
