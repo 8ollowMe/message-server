9507afe07baed3554b138eb2c68c8b756ed2816c
kjs0406 <rnjswlstjr19089@gmail.com>
2026-04-06 09:11:11 +0900

    feat : openfeign 의존성 추가

--- src/main/java/com/followMe/message_server/MessageServerApplication.java
+++ src/main/java/com/followMe/message_server/MessageServerApplication.java
@@ -2,10 +2,16 @@ package com.followMe.message_server;
 
 import org.springframework.boot.SpringApplication;
 import org.springframework.boot.autoconfigure.SpringBootApplication;
+import org.springframework.boot.autoconfigure.domain.EntityScan;
+import org.springframework.cloud.openfeign.EnableFeignClients;
+import org.springframework.data.jpa.repository.config.EnableJpaAuditing;
+import org.springframework.data.jpa.repository.config.EnableJpaRepositories;
 
 @EnableJpaAuditing
-@EnableFeignClients
 @SpringBootApplication
+@EnableFeignClients
+@EnableJpaRepositories(basePackages = "com.followMe")
+@EntityScan(basePackages = "com.followMe")
 public class MessageServerApplication {
 
 	public static void main(String[] args) {
