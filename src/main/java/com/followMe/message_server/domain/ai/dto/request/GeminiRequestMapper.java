6dea9bfe58d359b0ac00b6c62d374b954c80cc82
kjs0406 <rnjswlstjr19089@gmail.com>
2026-04-07 11:22:11 +0900

    feat : yml 오류 수정

---
+++ src/main/java/com/followMe/message_server/domain/ai/dto/request/GeminiRequestMapper.java
@@ -0,0 +1,21 @@
+package com.followMe.message_server.domain.ai.dto.request;
+
+import java.util.List;
+
+public class GeminiRequestMapper {
+
+    public static GeminiRequest from(AiHttpRequest request) {
+        return GeminiRequest.builder()
+                .contents(List.of(
+                        GeminiRequest.Content.builder()
+                                .parts(List.of(
+                                        GeminiRequest.Part.builder()
+                                                .text(request.getPrompt())
+                                                .build()
+                                ))
+                                .build()
+                ))
+                .build();
+    }
+}
+
